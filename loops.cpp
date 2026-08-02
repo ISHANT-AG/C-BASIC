2b8119f2ea579fc823b73519e97f7254df3dc7bc
Ishant Agarwal <AGARWALISHANT690@GMAIL.COM>
2026-08-02 23:10:09 +0530

    ishant

---
+++ loops.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     cout<<"Ishant"<<endl;
+//     cout<<"Ishant"<<endl;
+// }
+
+// for Loop
+// for(initialization; Condition; Update);
+
+int main()
+{
+    // // for
+    // for (int count = 1; count <= 10; count = count + 1)
+    // {
+    //     cout << "Ishant" << endl;
+    // }
+    int i;
+    for(int i = 51; i < 70; i = i + 1) {
+        cout << i << endl;
+    }
+
+}
+
+//Break Keyword
+//if(i==5){
+//break;
+//}
